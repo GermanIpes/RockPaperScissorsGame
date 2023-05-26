38ad22117f94ed3842c0c9a57568d447825b59d6
german-ipesriera <german.ipesriera@ukg.com>
2023-05-26 19:36:39 -0300

    updated game description

--- src/main/kotlin/services/impl/GameServiceImplementation.kt
+++ src/main/kotlin/services/impl/GameServiceImplementation.kt
@@ -9,7 +9,20 @@ class GameServiceImplementation(
     private val gameChallengeService: GameChallengeService
 ): GameService {
 
-    override fun generateNewGameValue(): GameValues = GameValues.ROCK
+    //Elimine el todo() que esta en la funcion generateNewGameValue()
+    //Reemplace ese valor con uno de los siguientes valores posibles:
+    //GameValues.ROCK
+    //GameValues.PAPER
+    //GameValues.SCISSORS
+    //GameValues.SPOCK
+    //GameValues.LIZARD
+    //En el caso de querer jugar un valor al azar, se puede utilizar:
+    //GameValues.values().random()
+
+    //Una vez finalizado, pulse el boton play verde que esta en la parte superior derecha del Ide
+
+    override fun generateNewGameValue(): GameValues =
+        TODO("Una vez finalizado, pulse el boton play verde que esta en la parte superior derecha del Ide")
 
     override fun createNewPlayer(): Player = Player(playerNumber = 1, gameValue = generateNewGameValue())
 
